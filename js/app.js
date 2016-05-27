bbb5d8e73bd05ee555c3a95c96afd626ddc1d3a1
chrisgits <cmannel77@gmail.com>
2016-05-27 15:52:25 -0500

    Updated to include Adding new items, deleting items - et. all
    Includes added 'Editing' tag to edited labels. Prior to scaffoding .js files into new folders

--- js/app.js
+++ js/app.js
@@ -1,21 +1,49 @@
+'use strict';
 //instantiate homeNet module
 //create controller within -- mainCtrl
 //creates $scope to controller
 angular.module('homeNet', [])
   //mainCtrl
-  .controller('mainCtrl', function($scope) {
-    $scope.learningNgChange = function() {
-      console.log("An input Changed");
+  .controller('mainCtrl', function($scope, dataService) {
+    $scope.addTodo = function() {
+      var todo = {name: "This is a new todo."};
+      $scope.todos.push(todo);
     };
 
-    $scope.todos = [
-      {"name": "clean the house"},
-      {"name": "water the lawn"},
-      {"name": "feed the dog"},
-      {"name": "pay the bills"},
-      {"name": "learn Angular"}
-    ];
+    dataService.getTodos(function(response) {
+      //console.log(response.data);
+      $scope.todos = response.data;
+      });
+  
+    $scope.deleteTodo = function(todo, $index) {
+      dataService.deleteTodo(todo);
+      $scope.todos.splice($index, 1);
+    };
+  
+    $scope.saveTodo = function(todo) {
+      dataService.saveTodo(todo);
+    };
 
   })
+    .service('dataService', function($http) {
+      this.getTodos = function(callback) {
+          $http.get('mock/todos.json')
+          .then(callback);
+        };
+      
+      this.deleteTodo = function(todo){
+        console.log("The " + todo.name + " task has been deleted!");
+        //simulate delete from DB.
+      };
+      
+      this.saveTodo = function(todo) {
+        
+      }
+      
+  
+  
+  
+      })
+    
 
 ;// homeNet module
